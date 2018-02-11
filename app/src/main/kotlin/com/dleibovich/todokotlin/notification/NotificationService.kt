8953fde3790452d2c052d1a9a916c2d5822bfaa1
david <ariedov@gmail.com>
2018-02-11 11:12:07 +0200

    tweaks notifications to dislpay inbox style

--- app/src/main/kotlin/com/dleibovich/todokotlin/notification/NotificationService.kt
+++ app/src/main/kotlin/com/dleibovich/todokotlin/notification/NotificationService.kt
@@ -1,11 +1,15 @@
 package com.dleibovich.todokotlin.notification
 
+import android.app.Notification
+import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.Service
 import android.content.Context
 import android.content.Intent
 import android.os.Binder
+import android.os.Build
 import android.os.IBinder
+import android.support.annotation.RequiresApi
 import android.support.v4.app.NotificationCompat
 import com.dleibovich.todokotlin.R
 import com.dleibovich.todokotlin.TodoApp
@@ -14,7 +18,7 @@ import java.util.*
 import javax.inject.Inject
 
 const val channel = "default"
-const val tag = "todoapp"
+const val notificationId = 1
 
 class NotificationService: Service(), Controller {
 
@@ -22,6 +26,7 @@ class NotificationService: Service(), Controller {
     lateinit var presenter: NotificationPresenter
 
     private lateinit var notificationManager: NotificationManager
+    private lateinit var notificationChannel: NotificationChannel
 
     override fun onCreate() {
         super.onCreate()
@@ -29,13 +34,26 @@ class NotificationService: Service(), Controller {
         (application as TodoApp)
                 .dataComponent()
                 .inject(this)
+
+        notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = createNotificationChannel(notificationManager)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager): NotificationChannel {
+        val notificationChannel = NotificationChannel(channel, getString(R.string.title_today),
+                NotificationManager.IMPORTANCE_DEFAULT)
+        notificationChannel.lockscreenVisibility = Notification.VISIBILITY_PUBLIC
+        notificationManager.createNotificationChannel(notificationChannel)
+        return notificationChannel
     }
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         if (intent != null) {
             val date = intent.getSerializableExtra(KEY_DATE) as Date
-
-            notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
 
             presenter.setController(this)
             presenter.updateList(date)
@@ -56,15 +74,17 @@ class NotificationService: Service(), Controller {
 
     override fun showNotification(items: List<TodoItem>) {
         notificationManager.cancelAll()
-        items.forEach { item ->
-            val notification = NotificationCompat.Builder(this, channel)
-                    .setSmallIcon(R.drawable.ic_notification)
-                    .setContentTitle(item.title)
-                    .setContentText(item.description)
-                    .setOngoing(true)
-                    .build()
-            notificationManager.notify(item.id, notification)
-        }
+
+        val style = NotificationCompat.InboxStyle()
+        items.forEach { item -> style.addLine(item.title) }
+
+        val notification = NotificationCompat.Builder(this, channel)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(getString(R.string.title_today))
+                .setStyle(style)
+                .setOngoing(true)
+                .build()
+        notificationManager.notify(notificationId, notification)
     }
 
     override fun showNoTasks() {
@@ -72,7 +92,7 @@ class NotificationService: Service(), Controller {
                 .setSmallIcon(R.drawable.ic_notification)
                 .setContentTitle(getString(R.string.no_plans_was_made))
                 .build()
-        notificationManager.notify(tag, 0, notification)
+        notificationManager.notify(notificationId, notification)
     }
 
     companion object {
